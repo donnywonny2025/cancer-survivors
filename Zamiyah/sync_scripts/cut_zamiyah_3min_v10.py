2a1ca69b030c4fd024dc1321fb31dca977ed212c
jeff kerr <jeffkerr@Mac.lan>
2026-03-29 05:23:48 -0400

    v18: Gap-aware breathing room, zero trailing bleed. Clean version bump.

--- Zamiyah/sync_scripts/cut_zamiyah_3min_v10.py
+++ Zamiyah/sync_scripts/cut_zamiyah_3min_v10.py
@@ -627,7 +627,7 @@ except Exception as lint_err:
     lint_ok = True
 
 # Write
-out_path = os.path.join(BASE, "Premiere/XML/Zamiyah_3min_Narrative_v17.xml")
+out_path = os.path.join(BASE, "Premiere/XML/Zamiyah_3min_Narrative_v18.xml")
 with open(out_path, "w") as fout:
     fout.write(xml_str)
 
